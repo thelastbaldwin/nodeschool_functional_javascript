ea54d4fab4ad8a965b586a9c58ddb0f69818919a
Steve Minor <stv.mnr@gmail.com>
2016-03-10 23:14:28 -0800

    solved, but maybe not the best way?

--- curry.js
+++ curry.js
@@ -1,12 +1,12 @@
 function curryN(fn, n) {
     n = n || fn.length;
-    return function curryIn(curryFunc, arg){
-        if(n-- > 1){
-            return curryIn.bind(null, curryFunc.bind(null, arg));
+    return function curryIn(curryFunc, count, arg){
+        if(count > 1){
+            return curryIn.bind(null, curryFunc.bind(null, arg), count-1);
         }else{
             return curryFunc(arg);
         }
-    }.bind(null, fn);
+    }.bind(null, fn, n);
 }
 
 module.exports = curryN
